242ecba4edf50b04b9654ac44660b046c694bc9b
Marshall Thompson <mdt_@gwu.edu>
2021-08-24 19:18:20 -0400

    refactor feature pooling

--- features.py
+++ features.py
@@ -6,62 +6,67 @@ from scipy.interpolate import interp1d
 from glob import glob
 from lib.utils import as_numpy
 import csv
+import pickle
 
-gpu = 0
-feat_2048 = torch.zeros(size=(1, 2048), device=torch.device('cuda:{}'.format(gpu)))
-feat_2048_whole = torch.zeros(size=(1, 2048, 38, 50), device=torch.device('cuda:{}'.format(gpu)))
-feat_150 = torch.Tensor
+# temp til better solution
+fc_feature = None
+pred_feature = None
 
 
+class Feature:
+    def __init__(self, fc_feature=fc_feature, pred_feature=pred_feature, device=0, num_classes=150, save_path=None, threshold=0.1):
+        self.device = torch.device('cuda:{}'.format(device))
+        self.num_classes = num_classes
+        self.save_path = save_path
+        self.threshhold = threshold
 
-def compute_image_representation(feat_150, feat_2048_whole, path, save=True):
-    cuda0 = torch.device('cuda:{}'.format(gpu))
-    
-    num_classes = 150
-    # size of each feat
-    size_2048 = feat_2048_whole.size()
-    size_150 = feat_150.size()
+        self.fc_feat = fc_feature.to(self.device).type(
+                torch.cuda.HalfTensor)
+        self.pred_feat = pred_feature.to(self.device).type(
+                torch.cuda.HalfTensor)
 
-    # features = torch.zeros((size_162[0], size_162[1]))
+        self.fc_feat_size = fc_feature.shape
+        self.fc_dim = fc_feature.shape[-1]
 
-    # most probable class at each pixel
-    _, feat_150_pred = torch.max(feat_150, dim=1)
-    feat_150_pred = as_numpy(feat_150_pred.squeeze(0))
+        self.pred_feat_size = pred_feature.shape
 
-    accepted_classes = [0 for i in range(num_classes)]
+    def compute_image_rep(self):
+        interp_feat = self.interpolate_feature(
+            self.pred_feat, (self.fc_feat_size[1], self.fc_feat_size[2]))
+        accepted_classes = self.get_accepted_classes(interp_feat)
+        computed_features = self.pool_feature(accepted_classes, interp_feat)
 
-    # find corresponding coords
-    x = nn.functional.interpolate(torch.from_numpy(feat_150_pred).unsqueeze(0).unsqueeze(0).type(torch.FloatTensor), size=(size_2048[2], size_2048[3]), mode='bilinear', align_corners=False)
-    x = x.squeeze().squeeze().type(torch.int64)
+    def pool_feature(self, accepted_classes, interp_feat):
+        for idx, acc in enumerate(accepted_classes):
+            locations = np.argwhere(
+                interp_feat.numpy() == accepted_classes[accepted_classes.index(acc)])
 
-    #get features with more than 10% of pixels
-    features_coords = torch.zeros((size_150[2], size_150[3], 1))
+            img_rep = torch.zeros((len(accepted_classes), self.fc_dim), device=self.device).type(
+                torch.cuda.HalfTensor)
+            for loc in locations:
+                img_rep = img_rep[idx].add(self.fc_feat[0][loc[0]][loc[1]])
+            img_rep[idx] = torch.div(img_rep[idx], len(locations))
+        return img_rep
 
-    threshold = 0.1; pix = x.size()[0] * x.size()[1]
-    accepted_classes = [i+1 for i in range(len(accepted_classes)) if (len(np.argwhere(feat_150_pred == i+1)) / pix) > threshold]
+    def get_accepted_classes(self, feature):
+        total_pixels = self.fc_feat_size[1] * self.fc_feat_size[2]
 
-    # # calculate average for each class in 2048d
-    feat_2048 = feat_2048_whole.permute((0,2,3,1)).type(torch.cuda.HalfTensor)
-    img_rep = torch.zeros((len(accepted_classes), 2048),device=cuda0).type(torch.cuda.HalfTensor)
-    for idx, cl in enumerate(accepted_classes):
-        coords = np.argwhere(x.numpy() == accepted_classes[accepted_classes.index(cl)])
-        for coord in coords:
-            img_rep[idx] = img_rep[idx].add(feat_2048[0][coord[0]][coord[1]])
-        img_rep[idx] = torch.div(img_rep[idx], len(coords))
-    if save:
-        torch.save(img_rep, os.path.join(path, "indiv_features1.pt"))
-        to_csv(accepted_classes, path)
-        return img_rep, accepted_classes
-    else:
-        return img_rep, accepted_classes
+        accepted_classes = []
+        for i in range(self.num_classes):
+            if (len(np.argwhere(feature.numpy() == i+1)) / total_pixels) > self.threshhold:
+                accepted_classes.append(i+1)
+        return accepted_classes
 
-def to_csv(accepted_classes, img_path):
-    """
-    Write new row in form of index, class, path
-    """
-    with open('data/test/features.csv', 'a+', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    def interpolate_feature(self, feature, size):
+        _, feat_150_pred = torch.max(feature, dim=1)
+        feat_150_pred = as_numpy(feat_150_pred.squeeze(0))
 
-        for i in range(len(accepted_classes)):
-            writer.writerow([i, accepted_classes[i], img_path])
+        interp_feature = nn.functional.interpolate(torch.from_numpy(feat_150_pred).unsqueeze(
+            0).unsqueeze(0).type(torch.FloatTensor), size=size, mode='bilinear', align_corners=False)
+        interp_feature = interp_feature.squeeze().squeeze().type(torch.int64)
+
+        return interp_feature
+
+    # TODO: SAVE THE FEATURES
+    def save_pooled_features(self, image_rep):
+        pass
